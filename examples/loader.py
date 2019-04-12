10e565b6de01cadf7c1907c441bf2cbe2a09a8a5
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-04-12 17:09:24 -0400

    getting bad alloc in python

---
+++ examples/loader.py
@@ -0,0 +1,28 @@
+import torch
+import cpploader
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', type=str)
+
+
+args = parser.parse_args()
+
+print('Setting up Image Folder')
+folder = cpploader.ImageFolder(args.data, True)
+
+print('Setting up  Loader')
+loader = cpploader.Loader(
+    folder,
+    128,
+    16,
+    2
+)
+
+for b in loader:
+    pass
+
+
+loader.report()
+
+
